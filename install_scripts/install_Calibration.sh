617a2b3443ad4adf8490f5920b387a58a392f223
xssun <xssun@umich.edu>
2024-01-05 01:24:25 +0000

    Update file install_Calibration.sh

--- install_scripts/install_Calibration.sh
+++ install_scripts/install_Calibration.sh
@@ -3,6 +3,11 @@
 # install ROS2 Camera Calibration package
 # https://navigation.ros.org/tutorials/docs/camera_calibration.html
 
+# downgrade setuptools version to 58.2.0
+# which is the last version to work with ros2 python packages without any warnings
+# https://robotics.stackexchange.com/questions/101255
+pip install setuptools==58.2.0
+
 sudo apt install ros-humble-camera-calibration-parsers
 sudo apt install ros-humble-camera-info-manager
 sudo apt install ros-humble-launch-testing-ament-cmake
