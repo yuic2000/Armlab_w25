39910d99005ec8a4c1d84ff2d7a3c6a3e5eca5d9
xssun <xssun@umich.edu>
2024-01-14 02:15:52 +0000

    Update file install_Interbotix.sh

--- install_scripts/install_Interbotix.sh
+++ install_scripts/install_Interbotix.sh
@@ -1,6 +1,11 @@
  #!/bin/bash
 #install interbotix-robot-arm
 
+# downgrade setuptools version to 58.2.0
+# which is the last version to work with ros2 python packages without any warnings
+# https://robotics.stackexchange.com/questions/101255
+pip install setuptools==58.2.0
+
 source /opt/ros/humble/setup.bash
 curl 'https://raw.githubusercontent.com/Interbotix/interbotix_ros_manipulators/main/interbotix_ros_xsarms/install/amd64/xsarm_amd64_install.sh' > xsarm_amd64_install.sh
 chmod +x xsarm_amd64_install.sh
